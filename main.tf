c9437b6aef0270358c0b9efb2d87ddc0519ee3f9
Bharathwaj-2001 <115392691+Bharathwaj-2001@users.noreply.github.com>
2026-05-14 21:12:01 +0530

    second commit

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_iam_role" "lambda_role" {
 
 # Attach basic policy
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_role.nam
+  role       = aws_iam_role.lambda_role.na
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
